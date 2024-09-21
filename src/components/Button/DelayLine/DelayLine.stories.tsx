ceb235638324efbb39b733655f39bbb714e785df
balckowl <shiggyjr1492@gmail.com>
2024-09-22 00:14:22 +0900

    add delayLine

---
+++ src/components/Button/DelayLine/DelayLine.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import Button from "./DelayLine"
+
+const meta = {
+    title: "Button",
+    component: Button,
+    parameters: {
+        layout: "centered"
+    }
+} satisfies Meta<typeof Button>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const DelayLine: Story = {}
---
+++ src/components/Button/DelayLine/DelayLine.tsx
@@ -0,0 +1,7 @@
+import styles from "./DelayLine.module.css"
+
+export default function DelayLine() {
+    return (
+        <div className={styles.btn}>Button</div>
+    )
+}
